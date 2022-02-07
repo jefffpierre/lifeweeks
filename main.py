8bcdb1fca6d035e684830e6018b418223fce2905
Jeff Pierre <49200624+jefffpierre@users.noreply.github.com>
2022-02-07 02:29:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year % 4 == 0:
+    print("Leap year")
+else:
+    print("Not leap year")
+
+
+
+
+
+
+
+
